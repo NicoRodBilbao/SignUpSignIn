7adee55b847adc0e3e93b6438ef42a80a56ebe47
joana-renteria <joana.rente6@tuta.io>
2022-10-25 10:01:52 +0200

    Update ApplicationWindowController.java
    small fixes

--- src/windowController/ApplicationWindowController.java
+++ src/windowController/ApplicationWindowController.java
@@ -1,14 +1,20 @@
 package windowController;
-
+        
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import model.User;
 
@@ -16,41 +22,44 @@ import model.User;
  *
  * @author joana, markel
  */
-public class ApplicationWindowController extends Application {
-	
+public class ApplicationWindowController {
+
+	@FXML
 	private User user;
+        @FXML
 	private Label lblWelcome;
+        @FXML
 	private Button btnLogOut;
-    
+        
+        private Stage stage;
+        
+        public void setStage(Stage stage) {
+            this.stage = stage;
+        }
+
 	public ApplicationWindowController(User user) {
 		this.user = user;
 	}
-	
+
 	public void initStage(Parent root) {
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setTitle("Welcome");
 		stage.setResizable(false);
-		stage.setOnShowing(this::setUsername);
-		btnLogOut.setOnAction(this::logOut);
+		stage.setOnShowing(this::handleWindowShowing);
+		btnLogOut.setOnAction(this::handleButtonLogOutAction);
 		stage.show();
 	}
-	
-	private void setUsername() {
+
+	private void handleWindowShowing(WindowEvent event) {
 		String welcomeText = lblWelcome.getText();
 		lblWelcome.setText(welcomeText.replace("Username", user.getLogin()));
+		btnLogOut.setTooltip(
+				new Tooltip("Pulsa para salir"));
 	}
-	
-	private void logOut() {
+
+	private void handleButtonLogOutAction() {
 		//TODO kill the app
 	}
-	
-    @Override
-    public void start(Stage primaryStage) {
-    	FXMLLoader loader = new FXMLLoader(
-    			getClass.getResource("ui/ApplicationWindow"));
-    	Parent root = (Parent) loader.load();
-    	
-    }
-    
+
 }
