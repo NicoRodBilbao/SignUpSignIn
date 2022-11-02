b8976ccc7ec31cf81e0ec33a059a5c9c8247ff4c
2dam <2dam@TartangaLH.eus>
2022-11-02 13:46:23 +0100

    Quick fixes

--- src/windowController/ApplicationWindowController.java
+++ src/windowController/ApplicationWindowController.java
@@ -1,52 +1,171 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package windowController;
 
-import javafx.application.Application;
-import static javafx.application.Application.launch;
+import exceptions.IncorrectUserException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import model.User;
 
 /**
+ * This class is the controller for the ApplicationWindow The window shows a
+ * greeting message to the logged in user
  *
- * @author nikol
+ * @author joana, markel
  */
-public class ApplicationWindowController extends Application {
+public class ApplicationWindowController {
+
+    private final Image dmImg = new Image("ui/sol_dark_mode.png");
+    private final Image lmImg = new Image("ui/sol_light_mode.png");
+    protected final Logger LOGGER = Logger.getLogger(SignUpWindowController.class.getName());
+
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    @FXML
+    private Pane paneMain;
+    @FXML
+    private User user;
+    @FXML
+    private Label lblWelcome;
+    @FXML
+    private Button btnLogOut;
+    @FXML
+    private Button btnDarkMode;
+    @FXML
+    private ImageView btnImgDarkMode;
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 
     /**
-     * @param args the command line arguments
+     * This method initializes the window.
+     *
+     * @param root There parent of all the children on the scene
      */
-    public static void main(String[] args) {
-        launch(args);
+    public void initStage(Parent root) {
+        LOGGER.info("Initializing the ApplicationWindow");
+        try {
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Welcome");
+            stage.setResizable(false);
+            stage.setOnShowing(this::handleWindowShowing);
+            btnLogOut.setOnAction(this::handleLogOutAction);
+            btnDarkMode.setOnAction(this::handleDarkModeButtonAction);
+            stage.setOnCloseRequest(this::handleExitAction);
+            stage.show();
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
     }
-    
+
+    private void handleWindowShowing(WindowEvent event) {
+        LOGGER.info("Beggining ApplicationWindowController::handleWindowShowing");
+        try {
+            String welcomeText = lblWelcome.getText();
+            lblWelcome.setText(welcomeText.replace("Username", user.getLogin()));
+            btnLogOut.setTooltip(
+                    new Tooltip("Pulsa para salir"));
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
+
+    private void handleDarkModeButtonAction(ActionEvent event) {
+        LOGGER.info("Beggining ApplicationWindowController::handleDarkModeButtonAction");
+        try {
+            int i = 53, j = 18;
+            // Si pulsas el botón, comprueba el tema que tiene la ventana. 
+            // En caso de que la imagen sea sol_dark_mode, se cambiará por el sol_light_mode.
+            if (btnImgDarkMode.getImage().getPixelReader().getArgb(i, j) == dmImg.getPixelReader().getArgb(i, j)) {
+                btnImgDarkMode.setImage(lmImg);
+                lblWelcome.setTextFill(Color.BLACK);
+                // El fondo cambia el color de #333333, a WHITE.
+                paneMain.setStyle("-fx-background-color:WHITE");
+                btnDarkMode.setStyle("-fx-background-color:WHITE");
+            } // En caso de que la imagen sea sol_light_mode, se cambiará por el sol_dark_mode.
+            else {
+                btnImgDarkMode.setImage(dmImg);
+                lblWelcome.setTextFill(Color.WHITE);
+                paneMain.setStyle("-fx-background-color:#333333");
+                btnDarkMode.setStyle("-fx-background-color:#333333");
+            }
+        } catch (Exception e) {
+            showError(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void handleLogOutAction(ActionEvent event) {
+        LOGGER.info("Beggining ApplicationWindowController::handleLogOutAction");
+        try {
+            // Carga el document FXML y obtiene un objeto Parent
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/ui/LogInWindow.fxml"));
+            Parent root = (Parent) loader.load();
+
+            LogInWindowController controller
+                    = (LogInWindowController) loader.getController();
+            controller.setStage(stage);
+            controller.initStage(root);
+        } catch (IOException e) {
+            showError(e.getMessage());
+        }
+    }
+
+    private void handleExitAction(WindowEvent event) {
+        LOGGER.info("Beggining ApplicationWindowController::handleExitAction");
+        try {
+            Platform.exit();
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
+
+    private void showError(String msg) {
+        LOGGER.log(Level.SEVERE, "ERROR: {0}", msg);
+        Alert alert = new Alert(Alert.AlertType.ERROR, msg);
+        alert.showAndWait()
+                .filter(response -> response == ButtonType.OK);
+    }
+
+    /**
+     * This methods allows for passing a user to the window controller, so the
+     * their data can be shown
+     *
+     * @param u The user that has logged in
+     */
+    public void initData(User u) {
+        LOGGER.info("Beggining ApplicationWindowController::initData");
+        try {
+            //TODO Parametrizar excepciones
+            if (u == null) {
+                throw new IncorrectUserException();
+            }
+            this.user = u;
+        } catch (IncorrectUserException e) {
+            showError(e.getMessage());
+        }
+    }
+
 }
--- src/windowController/SignUpWindowController.java
+++ src/windowController/SignUpWindowController.java
@@ -1,52 +1,360 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package windowController;
 
-import javafx.application.Application;
-import static javafx.application.Application.launch;
+import exceptions.*;
+import factories.UserFactory;
+import interfaces.Userable;
+import java.io.IOException;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import model.User;
+import model.UserPrivilege;
+import model.UserStatus;
 
 /**
+ * This window has the functionality of singing up a user on the program
  *
- * @author nikol
+ * @author Nicolas Rodriguez / Emil Nuñez
  */
-public class SignUpWindowController extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+public class SignUpWindowController {
+
+    private Userable dao = UserFactory.getAccessUser();
+    private Stage stage;
+    private final Image dmImg = new Image("ui/sol_dark_mode.png");
+    private final Image lmImg = new Image("ui/sol_light_mode.png");
+    protected final Logger LOGGER = Logger.getLogger(SignUpWindowController.class.getName());
+    @FXML
+    private Pane paneMain;
+    @FXML
+    private Label lblUsername, lblFullName, lblEmail, lblPassword, lblRepeatPassword;
+    @FXML
+    private TextField tfUsername, tfEmail, tfFullName;
+    @FXML
+    private PasswordField tfPassword, tfRepeatPassword;
+    @FXML
+    private Button btnSignUp, btnGoBack, btnDarkMode;
+    @FXML
+    private Separator decorUsername, decorEmail, decorPassword, decorFullName, decorRepeatPassword;
+    @FXML
+    private ImageView btnImgDarkMode;
+
+    /**
+     * This method initializes the window.
+     *
+     * @param root There parent of all the children on the scene
+     */
+    public void initStage(Parent root) {
+        LOGGER.info("Initializing SignUp stage.");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        // , se le pondrá el título de “Sign Up” a la ventana
+        stage.setTitle("SignUp");
+        //  será una ventana no resizable
+        stage.setResizable(false);
+        // Listens to the event of the window showing up
+        stage.setOnShowing(this::handleSignUpWindowShowing);
+        tfUsername.focusedProperty().addListener(this::focusChanged);
+        tfEmail.focusedProperty().addListener(this::focusChanged);
+        tfFullName.focusedProperty().addListener(this::focusChanged);
+        tfPassword.focusedProperty().addListener(this::focusChanged);
+        tfRepeatPassword.focusedProperty().addListener(this::focusChanged);
+        tfUsername.textProperty().addListener((event) -> this.textChange(KeyEvent.KEY_TYPED));
+        tfEmail.textProperty().addListener((event) -> this.textChange(KeyEvent.KEY_TYPED));
+        tfFullName.textProperty().addListener((event) -> this.textChange(KeyEvent.KEY_TYPED));
+        tfPassword.textProperty().addListener((event) -> this.textChange(KeyEvent.KEY_TYPED));
+        tfRepeatPassword.textProperty().addListener((event) -> this.textChange(KeyEvent.KEY_TYPED));
+        stage.show();
+
+    }
+
+    private void textChange(EventType<KeyEvent> KEY_TYPED) {
+        if (!tfUsername.getText().trim().isEmpty()
+                && !tfEmail.getText().trim().isEmpty()
+                && !tfFullName.getText().trim().isEmpty()
+                && !tfPassword.getText().trim().isEmpty()
+                && !tfRepeatPassword.getText().trim().isEmpty()) {
+            btnSignUp.setDisable(false);
+        }
+        if (tfUsername.getText().trim().isEmpty()
+                || tfEmail.getText().trim().isEmpty()
+                || tfFullName.getText().trim().isEmpty()
+                || tfPassword.getText().trim().isEmpty()
+                || tfRepeatPassword.getText().trim().isEmpty()) {
+            btnSignUp.setDisable(true);
+        }
+    }
+
+    private boolean validateFieldString(String txtValidate) {
+        Boolean flag = true;
+        if (txtValidate.contains(" ") || txtValidate.length() >= 30) {
+            flag = !flag;
+        }
+        return flag;
+    }
+
+    private boolean validateEmail(String txtEmail) { // TODO validar patrón de Email
+        return true;
+    }
+
+    @FXML
+    /**
+     * This method validates the TextfFields and executes signUp() if ir works
+     * propperly.
+     *
+     * @param event The observed event
+     */
+    private void handleSignUpButtonAction(ActionEvent event) {
+        /*Se validará el campo tfUsername.
+            En caso de que no se valide con más
+            de 30 caracteres o que haya
+            espacios en blanco, lanza
+            IncorrectUserException.*/
+        try {
+            if (!validateFieldString(tfUsername.getText())) {
+                decorUsername.setStyle("-fx-background-color:RED;");
+                throw new IncorrectUserException(); // tfUsername fails !!parameterize exception
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            /* Se validará el campo tfPassword.
+                En caso de que no valide con más de
+                30 caracteres o que haya espacios
+                en blanco, lanza
+                IncorrectPasswordException*/
+            if (validateFieldString(tfPassword.getText())) {
+                decorPassword.setStyle("-fx-background-color:RED;");
+                throw new IncorrectPasswordException(); // tfPassword fails !!parameterize exception 
+            }
+            /*Se validará el campo de tfRepeatPassword.
+                    En caso de que no valide con más de
+                    30 caracteres o que haya espacios
+                    en blanco, lanza
+                    IncorrectPasswordException.*/
+            if (validateFieldString(tfRepeatPassword.getText())) {
+                decorRepeatPassword.setStyle("-fx-background-color:RED;");
+                throw new IncorrectPasswordException(); // tfRepeatPassword fails !!parameterize exception
+            }
+            /*Se validará el campo tfEmail.
+                        En caso de que no sea válido con un
+                        formato correcto, lanza
+                        EmailAlreadyExistsException.*/
+            if (validateEmail(tfEmail.getText())) {
+                decorEmail.setStyle("-fx-background-color:RED;");
+                throw new IncorrectEmailException(); // tfEmail fails
+            }
+            /*Se validará que la tfPassword y
+                            tfRepeatPassword sean iguales.
+                            En el caso de que el tfPassword
+                            como el tfRepeatPassword sean
+                            distintos, lanza IncorrectPasswordException.*/
+            if (tfPassword.getText().equals(tfRepeatPassword.getText())) {
+                decorPassword.setStyle("-fx-background-color:RED;");
+                decorRepeatPassword.setStyle("-fx-background-color:RED;");
+                throw new PasswordDoesntMatchException(); // tfPassword and tfRepeatPassword don't contain the same string !!parameterize exception
+            }
+            /*Se validará el campo de tfFullName.
+                                En caso de que no valide con más de
+                                50 caracteres lanza
+                                IncorrectUserException.*/
+            if (tfFullName.getText().length() <= 50) {
+                decorFullName.setStyle("-fx-background-color:RED;");
+                throw new IncorrectFullNameException(); // tfFullName fails !!parameterize exception
+            }
+            //Integer id, String login, String email, String fullName, UserStatus status, UserPrivilege privilege, String password
+            User user = new User(0, // generic id
+                    tfUsername.getText(),
+                    tfEmail.getText(),
+                    tfFullName.getText(),
+                    UserStatus.ENABLED,
+                    UserPrivilege.USER,
+                    tfPassword.getText());
+            dao.signUp(user);
+            new Alert(Alert.AlertType.INFORMATION, "The user has been successfully registered.", ButtonType.OK).showAndWait();
+            //En caso de que alguna validación sea errónea. 
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK).showAndWait();
+        }
+    }
+
+    @FXML
+    /**
+     * This method closes this window and returns to LogInWindow
+     *
+     * @param event The observed event
+     */
+    private void handleGoBackButtonAction(ActionEvent event) {  //! TODO probar el funcionamiento con las ventanas de otros integrantes
+        try {
+            Stage secondaryStage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/LogInWindow.fxml"));
+            // Carga el document FXML y obtiene un objeto Parent
+            Parent root = (Parent) loader.load();
+
+            LogInWindowController controller
+                    = ((LogInWindowController) loader.getController());
+            controller.setStage(secondaryStage);
+            controller.initStage(root);
+            stage.close();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK).showAndWait();
+        }
+    }
+
+    @FXML
+    /**
+     * This method changes atributes of the window in order to change the visual
+     * theme
+     *
+     * @param event The observed event
+     */
+    private void handleDarkModeButtonAction(ActionEvent event) {
+        int i = 53, j = 18;
+        // Si pulsas el botón, comprueba el tema que tiene la ventana. 
+        // En caso de que la imagen sea sol_dark_mode, se cambiará por el sol_light_mode.
+        if (btnImgDarkMode.getImage().getPixelReader().getArgb(i, j) == dmImg.getPixelReader().getArgb(i, j)) {
+            btnImgDarkMode.setImage(lmImg);
+            // Los background de los tfUsername, tfEmail, tfFullName, tfPassword y tfRepeatPassword cambiarán de #3A3A3A a #DDDDDD
+            tfUsername.setBackground(new Background(new BackgroundFill(Color.valueOf("#DDDDDD"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfEmail.setBackground(new Background(new BackgroundFill(Color.valueOf("#DDDDDD"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfFullName.setBackground(new Background(new BackgroundFill(Color.valueOf("#DDDDDD"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfPassword.setBackground(new Background(new BackgroundFill(Color.valueOf("#DDDDDD"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfRepeatPassword.setBackground(new Background(new BackgroundFill(Color.valueOf("#DDDDDD"), CornerRadii.EMPTY, Insets.EMPTY)));
+            // y la letra pasará de WHITE a BLACK.
+            tfUsername.setStyle("-fx-text-inner-color:BLACK");
+            tfEmail.setStyle("-fx-text-inner-color:BLACK");
+            tfFullName.setStyle("-fx-text-inner-color:BLACK");
+            tfPassword.setStyle("-fx-text-inner-color:BLACK");
+            tfRepeatPassword.setStyle("-fx-text-inner-color:BLACK");
+            // Las label lblUsername, lblEmail, lblFullName, lblPassword, 
+            //lblRepeatPassword cambiaran de color de la letra de WHITE a BLACK.
+            lblUsername.setTextFill(Color.BLACK);
+            lblEmail.setTextFill(Color.BLACK);
+            lblFullName.setTextFill(Color.BLACK);
+            lblPassword.setTextFill(Color.BLACK);
+            lblRepeatPassword.setTextFill(Color.BLACK);
+            // El fondo cambia el color de #333333, a WHITE.
+            paneMain.setStyle("-fx-background-color:WHITE");
+            btnDarkMode.setStyle("-fx-background-color:WHITE");
+        } // En caso de que la imagen sea sol_light_mode, se cambiará por el sol_dark_mode.
+        else {
+            btnImgDarkMode.setImage(dmImg);
+            // En caso contrario, los background de los tfUsername, tfEmail, tfFullName, tfPassword y tfRepeatPassword cambiarán de #DDDDDD a #3A3A3A
+            tfUsername.setBackground(new Background(new BackgroundFill(Color.valueOf("#3A3A3A"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfEmail.setBackground(new Background(new BackgroundFill(Color.valueOf("#3A3A3A"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfFullName.setBackground(new Background(new BackgroundFill(Color.valueOf("#3A3A3A"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfPassword.setBackground(new Background(new BackgroundFill(Color.valueOf("#3A3A3A"), CornerRadii.EMPTY, Insets.EMPTY)));
+            tfRepeatPassword.setBackground(new Background(new BackgroundFill(Color.valueOf("#3A3A3A"), CornerRadii.EMPTY, Insets.EMPTY)));
+            // la letra pasará de BLACK a WHITE
+            tfUsername.setStyle("-fx-text-inner-color:WHITE");
+            tfEmail.setStyle("-fx-text-inner-color:WHITE");
+            tfFullName.setStyle("-fx-text-inner-color:WHITE");
+            tfPassword.setStyle("-fx-text-inner-color:WHITE");
+            tfRepeatPassword.setStyle("-fx-text-inner-color:WHITE");
+            // En caso contrario, las label lblUsername, lblEmail, lblFullName, 
+            // lblPassword, lblRepeatPassword cambiaran de color de la letra de BLACK a WHITE.
+            lblUsername.setTextFill(Color.WHITE);
+            lblEmail.setTextFill(Color.WHITE);
+            lblFullName.setTextFill(Color.WHITE);
+            lblPassword.setTextFill(Color.WHITE);
+            lblRepeatPassword.setTextFill(Color.WHITE);
+            // En caso contrario, pasará a #333333
+            paneMain.setStyle("-fx-background-color:#333333");
+            btnDarkMode.setStyle("-fx-background-color:#333333");
+        }
+
     }
 
     /**
-     * @param args the command line arguments
+     * This method is used when a listener is triggered by focusing on a field
+     *
+     * @param observable The value listened
+     * @param oldValue The old value obtained
+     * @param newValue The new value obtained
      */
-    public static void main(String[] args) {
-        launch(args);
+    private void focusChanged(ObservableValue observable, Boolean oldValue, Boolean newValue) {
+        if (newValue) {
+            if (btnDarkMode.isFocused()) {
+                System.err.println("A");
+            }
+            if (tfUsername.isFocused()) {
+                decorUsername.setStyle("-fx-background-color:AQUA;");
+            } else if (tfEmail.isFocused()) {
+                decorEmail.setStyle("-fx-background-color:AQUA;");
+            } else if (tfFullName.isFocused()) {
+                decorFullName.setStyle("-fx-background-color:AQUA;");
+            } else if (tfPassword.isFocused()) {
+                decorPassword.setStyle("-fx-background-color:AQUA;");
+            } else if (tfRepeatPassword.isFocused()) {
+                decorRepeatPassword.setStyle("-fx-background-color:AQUA;");
+            }
+        }
+        if (oldValue) {
+            if (!tfUsername.isFocused()) {
+                decorUsername.setStyle("-fx-background-color:GRAY;");
+            }
+            if (!tfEmail.isFocused()) {
+                decorEmail.setStyle("-fx-background-color:GRAY;");
+            }
+            if (!tfFullName.isFocused()) {
+                decorFullName.setStyle("-fx-background-color:GRAY;");
+            }
+            if (!tfPassword.isFocused()) {
+                decorPassword.setStyle("-fx-background-color:GRAY;");
+            }
+            if (!tfRepeatPassword.isFocused()) {
+                decorRepeatPassword.setStyle("-fx-background-color:GRAY;");
+            }
+        }
     }
-    
+
+    /**
+     * This method initializes the window showing state.
+     *
+     * @param event The event listened
+     */
+    private void handleSignUpWindowShowing(WindowEvent event) {
+        LOGGER.info("Beggining SignInWindowController::handleSignUpWindowShowing");
+        //  btnSignUp estará deshabilitado
+        btnSignUp.setDisable(true);
+        //  el btnGoBack estará habilitado
+        btnGoBack.setDisable(false);
+        //  y el foco estará en el campo tfUsername
+        tfUsername.requestFocus();
+        // los campos vacíos
+        tfUsername.setText("");
+        tfEmail.setText("");
+        tfFullName.setText("");
+        tfPassword.setText("");
+        tfRepeatPassword.setText("");
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
 }
